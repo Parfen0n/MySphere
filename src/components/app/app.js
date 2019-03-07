3712c3843caff7cfb6c6477971bc4badccdd453a
Parfen0n <parfenon666@gmail.com>
2019-03-07 16:18:13 +0600

    Added components app and header; Create css layout for app and header

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Header from '../header/';
+
+import './app.css';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className="my-sphere-app">
+                <Header />
+            </div>
+        )
+    }
+}
---
+++ src/components/header/header.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+import './header.css'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <header className="header">
+                <div className="logo">
+                    <a href="#/main">My Sphere</a>
+                </div>
+                <nav>
+                    <li>
+                        <a href="#/main">Home</a>
+                    </li>
+                    <li>
+                        <a href="#/main">Vacansies</a>
+                    </li>
+                    <li>
+                        <a href="#/main">Specialists</a>
+                    </li>
+                    <li>
+                        <a href="#/main">Companies</a>
+                    </li>
+                </nav>
+                <div className="login">
+                    <a href="#/signup">Sign in</a>
+                    <button type="button">
+                        Sign up
+                    </button>
+                </div>
+            </header>
+        )
+    }
+}
