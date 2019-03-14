679c1d7f3d5e6592dc79d6ec7fe888e6ca5f2520
Yeldan <yeldan99@gmail.com>
2019-03-14 23:33:07 +0600

    Created login, registration, main pages and footer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
+//import App from './components/app';
+//import Login from './components/login';
+//import Registration from './components/registration';
+import Main from './components/main';
 
-ReactDOM.render(<App />,
+//ReactDOM.render(<App />,
+//  document.getElementById('root'));
+
+//ReactDOM.render(<Login/>,
+//  document.getElementById('root'));
+
+//ReactDOM.render(<Registration/>,
+//  document.getElementById('root'));
+
+ReactDOM.render(<Main/>,
   document.getElementById('root'));
