e18d70e868631cdae66a13d3dbdcc971e0c32cf1
Yeldan <yeldan99@gmail.com>
2019-03-29 22:19:02 +0600

    Add files via upload

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,99 +1,28 @@
 import React, { Component } from 'react';
 
-import Header from '../header';
-import Filter from '../filter'
-import RandomVacancy from '../random-vacancy';
-import ItemList from '../item-list';
-import Pagination from '../pagination';
-import Footer from '../footer';
+import Login from '../components/login';
+import Registration from '../components/registration';
+import Vacancy from '../components/vacancy-page';
+import Main from '../components/main';
 
 import './app.css';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 export default class App extends Component {
 
-    state = {
-        vacancies: [
-            {
-                title: 'Backend developer',
-                special: 'Backend',
-                grade: 'Middle',
-                skills: [
-                    'PHP', 'Jquery', 'HTML',
-                    'CSS', 'Wordpress'
-                ],
-                company: 'Email Soldiers',
-                city: 'Ryazan',
-                graphic: 'Full Day',
-                salary: '35 000',
-                date: "08.09.11"
-            },
-            {
-                title: 'frontend developer',
-                special: 'frontend',
-                grade: 'junior',
-                skills: [
-                    'JS', 'Jquery', 'HTML',
-                    'CSS', 'Wordpress'
-                ],
-                company: 'Soldiers',
-                city: 'Moskow',
-                graphic: 'Half Day',
-                salary: '25 000',
-                date: "08.09.11"
-            },
-            {
-                title: 'testing',
-                special: 'Testing',
-                grade: 'Senior',
-                skills: [
-                    'Jest', 'Webpack', 'HTML',
-                    'CSS', 'Wordpress'
-                ],
-                company: 'Email',
-                city: 'Almaty',
-                graphic: 'Full Day',
-                salary: '100 000',
-                date: "08.09.11"
-            }
-        ],
-        term: ''
-    }
-
-    onSearchChange = (value) => {
-        this.setState({
-            term: value
-        })
-    }
-
-    search(items, term) {
-        if(term.length === 0) {
-            return items;
-        }
-
-        return items.filter((item) => {
-            return item.skills.join().toLowerCase().indexOf(term.toLowerCase()) > -1  || item.title.toLowerCase().indexOf(term.toLowerCase()) > -1 ;
-        })
-    }
- 
     render() {
-        const { vacancies, term } = this.state;
-
-        const searchedVacancies = this.search(vacancies, term);
-        const numberVacancies = searchedVacancies.length
 
         return (
-            <div className="my-sphere-app">
-                <Header />
-                <div className="content">
-                    <RandomVacancy />
-                    <Filter />
-                    <ItemList items={searchedVacancies} 
-                              numberOfVacancies={numberVacancies}
-                              onSearchChange={this.onSearchChange}/>
-                    <Pagination />
-                </div>
-                <Footer />
-            </div>
-        )
+            <Router>
+                
+                <Route path="/" component={Main} />
+                <Route path="/vacancy" component={Vacancy} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Registration} />
+
+            </Router>
+        );
     }
+
 }
